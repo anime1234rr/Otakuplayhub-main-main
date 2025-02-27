491f6be05e43a07cc71db49f79e31ae7eb9dd030
anime1234rr <arandaa196@gmail.com>
2025-02-27 19:22:04 +0100

    Agregar archivos iniciales y configuración del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; // El plugin para Vite
+import cloudflare from '@astrojs/cloudflare';  
+import tailwindcss from '@tailwindcss/vite';  
 
 // https://astro.build/config
 export default defineConfig({
+ 
   vite: {
-    plugins: [tailwindcss()], // Solo necesitas esto para integrar Tailwind
+    plugins: [tailwindcss()],  
   },
+
+
+ adapter: cloudflare({
+  imageService: 'cloudflare'
+}),
+
+
+output: 'server', 
 });
